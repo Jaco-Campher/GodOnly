20ffe4fe8d951edceb30de11a17b86911ff0210b
Jaco Campher <jaco.campher@exponant.com>
2024-02-26 11:49:08 +0200

    Added paging to the Prophesy Legend.

--- Go/ToolsPages/Legend/Legend.js
+++ Go/ToolsPages/Legend/Legend.js
@@ -2,22 +2,88 @@ var GO;
 (function (GO) {
     var Tools;
     (function (Tools) {
-        class LegendViewModel {
+        class Paging {
+            constructor() {
+                this.PageSize = ko.observable(20);
+                this.CurrentPageIndex = ko.observable(0);
+                this.Items = ko.observableArray([]);
+                this.Pages = ko.observableArray([]);
+                this.CurrentItems = ko.computed(() => {
+                    let maxPageIndex = Math.ceil(ko.unwrap(this.Items).length / this.PageSize()) - 1;
+                    if (this.CurrentPageIndex() > maxPageIndex) {
+                        this.CurrentPageIndex(0);
+                    }
+                    let startIndex = this.PageSize() * this.CurrentPageIndex();
+                    return ko.unwrap(this.Items).slice(startIndex, startIndex + Number(this.PageSize()));
+                }, this);
+                this.PrevPage = () => {
+                    if (this.CurrentPageIndex() == 0) {
+                        return;
+                    } //Already on the first page.
+                    this.CurrentPageIndex(this.CurrentPageIndex() - 1);
+                    GO.ScrollToTop();
+                };
+                this.NextPage = () => {
+                    let maxPageIndex = Math.ceil(ko.unwrap(this.Items).length / this.PageSize()) - 1;
+                    if (this.CurrentPageIndex() == maxPageIndex) {
+                        return;
+                    } //Already on the last page.
+                    this.CurrentPageIndex(this.CurrentPageIndex() + 1);
+                    GO.ScrollToTop();
+                };
+                this.UpdatePageIndex = (index) => {
+                    this.CurrentPageIndex(index);
+                    GO.ScrollToTop();
+                };
+                this.UpdatePageNames = () => {
+                    let pageButtons = [];
+                    let maxPageIndex = Math.ceil(ko.unwrap(this.Items).length / this.PageSize()) - 1;
+                    for (let pageIndex = 0; pageIndex <= maxPageIndex; pageIndex++) {
+                        let first = pageIndex * this.PageSize();
+                        let last = (first + this.PageSize()) - 1;
+                        if (last >= this.Items().length) {
+                            last = this.Items().length - 1;
+                        }
+                        pageButtons.push(new PageButton(`${this.Items()[first].PageTitle} - ${this.Items()[last].PageTitle}`, pageIndex));
+                    }
+                    this.Pages(pageButtons);
+                };
+            }
+        }
+        Tools.Paging = Paging;
+        class PageItem {
+            constructor() {
+                this.PageTitle = '';
+            }
+        }
+        Tools.PageItem = PageItem;
+        class PageButton {
+            constructor(Title, Index) {
+                this.Title = Title;
+                this.Index = Index;
+            }
+        }
+        Tools.PageButton = PageButton;
+        class LegendViewModel extends Paging {
+            //Ledgends: KnockoutObservableArray<GO.iLegend> = ko.observableArray<GO.iLegend>([]);
             //****************************************************************************
             // Constructor
             //****************************************************************************
             constructor() {
-                this.Ledgends = ko.observableArray([]);
+                super();
                 this.LoadLegends();
             }
-            async LoadLegends() {
+            LoadLegends() {
                 for (let legendName in go.LegendsObject) {
                     let legend = go.LegendsObject[legendName];
                     if (legend.Lookup == undefined) {
                         legend.Name = legendName;
-                        this.Ledgends.push(legend);
+                        legend.PageTitle = legendName;
+                        //this.Ledgends.push(legend);
+                        this.Items.push(legend);
                     }
                 }
+                this.UpdatePageNames();
             }
         }
         Tools.LegendViewModel = LegendViewModel;
--- Go/ToolsPages/Legend/Legend.ts
+++ Go/ToolsPages/Legend/Legend.ts
@@ -1,27 +1,101 @@
 ﻿namespace GO.Tools {
 
-    export class LegendViewModel {
 
-        Ledgends: KnockoutObservableArray<GO.iLegend> = ko.observableArray<GO.iLegend>([]);
+    export class Paging<TI extends PageItem> {
+        PageSize: KnockoutObservable<number> = ko.observable(20);
+        CurrentPageIndex: KnockoutObservable<number> = ko.observable(0);
+
+        Items: KnockoutObservableArray<TI> = ko.observableArray<TI>([]);
+        Pages: KnockoutObservableArray<PageButton> = ko.observableArray<PageButton>([]);
+
+        CurrentItems: KnockoutComputed<Array<TI>> = ko.computed((): Array<TI> => {
+            let maxPageIndex: number = Math.ceil(ko.unwrap(this.Items).length / this.PageSize()) - 1;
+
+            if (this.CurrentPageIndex() > maxPageIndex) { this.CurrentPageIndex(0); }
+            let startIndex: number = this.PageSize() * this.CurrentPageIndex();
+
+            return ko.unwrap<Array<TI>>(this.Items).slice(startIndex, startIndex + Number(this.PageSize()));
+        }, this);
+
+
+        PrevPage = () => {
+            if (this.CurrentPageIndex() == 0) { return; } //Already on the first page.
+            this.CurrentPageIndex(this.CurrentPageIndex() - 1);
+
+            GO.ScrollToTop();
+        }
+
+        NextPage = () => {
+            let maxPageIndex: number = Math.ceil(ko.unwrap(this.Items).length / this.PageSize()) - 1;
+
+            if (this.CurrentPageIndex() == maxPageIndex) { return; } //Already on the last page.
+            this.CurrentPageIndex(this.CurrentPageIndex() + 1);
+
+            GO.ScrollToTop();
+        }
+
+        UpdatePageIndex = (index: number) => {
+            this.CurrentPageIndex(index);
+
+            GO.ScrollToTop();
+        }
+
+        UpdatePageNames = () => {
+            let pageButtons: Array<PageButton> = [];
+
+            let maxPageIndex: number = Math.ceil(ko.unwrap(this.Items).length / this.PageSize()) - 1;
+
+            for (let pageIndex = 0; pageIndex <= maxPageIndex; pageIndex++) {
+                let first: number = pageIndex * this.PageSize();
+                let last: number = (first + this.PageSize()) - 1;
+
+                if (last >= this.Items().length) { last = this.Items().length - 1 }
+
+                pageButtons.push(new PageButton(`${this.Items()[first].PageTitle} - ${this.Items()[last].PageTitle}`, pageIndex));
+            }
+
+            this.Pages(pageButtons);
+        }
+    }
+
+    export class PageItem {
+        PageTitle?: string = '';
+    }
+
+    export class PageButton {
+        constructor(public Title: string, public Index: number) { }
+    }
+
+
+    export class LegendViewModel extends Paging<GO.iLegend> {
+
+        //Ledgends: KnockoutObservableArray<GO.iLegend> = ko.observableArray<GO.iLegend>([]);
 
         //****************************************************************************
         // Constructor
         //****************************************************************************
         constructor() {
+            super();
             this.LoadLegends();
         }
 
-        async LoadLegends() {
+        LoadLegends() {
             for (let legendName in go.LegendsObject) {
                 let legend: GO.iLegend = go.LegendsObject[legendName];
                 
                 if (legend.Lookup == undefined) {
                     legend.Name = legendName;
+                    legend.PageTitle = legendName;
 
-                    this.Ledgends.push(legend);
+                    //this.Ledgends.push(legend);
+                    this.Items.push(legend);
                 }
             }
+
+            this.UpdatePageNames();
         }
 
     }
+
+    
 }
